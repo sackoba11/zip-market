3961f0d99ee5b8f6ff95f6a6fa79202bfc33f131
sackoba <badrasacko11@gmail.com>
2025-05-05 16:03:03 +0000

    completed utility folders

---
+++ lib/utils/constants/api_constants.dart
@@ -0,0 +1,9 @@
+/* -- LIST OF Constants used in APIs -- */
+
+// Example
+class ZMAPIConstants {
+  static const String baseUrl = 'https://api.zm.com';
+  static const String apiKey = 'your_api;key_here';
+  static const String tSecretAPIKey =
+      "cwt_live_b2da6ds3df3e785v8ddc59198f7615ba";
+}
